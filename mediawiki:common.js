3dd502793c082a310406499f9a4ae6dd70d6b143
Manuel Huber <ManuelHu@users.noreply.github.com>
2016-09-15 16:00:24 +0200

    Create mediawiki:common.js

---
+++ mediawiki:common.js
@@ -0,0 +1,25 @@
+if ( mw.config.get( 'wgPageName' ) === 'Crew' ) {
+  var url = "https://raw.githubusercontent.com/ManuelHu/octoawesome/crew/OctoAwesome/OctoAwesome.Client/Assets/OctoAwesome.Client/Crew/crew.xml";
+  $.ajax({
+    url: url
+  }).done(function (d) {
+    var xml = $.parseXML(d);
+    var $xml = $( xml );
+    $xml.find('CrewMember').each(function() {
+      var username    = $(this).find('Username').text();
+      var alias       = $(this).find('Alias').text();
+      var description = $(this).find('Description').text();
+      var achievements = [];
+      $(this).find('Achievements').each(function() {
+        achievements.push($(this).text());
+      });
+      var links = [];
+      $(this).find('Link').each(function() {
+        links.push('<a rel="nofollow" class="external text" href="'+$(this).attr('Url')+'">'+$(this).attr('Title')+'</a>');
+      });
+      
+      var content = '<h2><span class="mw-headline" id="'+username+'">'+username+'</span></h2><p>'+description+'</p><p><b>Weiterführende Links:</b> '+links.join(' | ')+'<br /><b>Achievements:</b> '+achievements.join(' | ')+'</p>';
+      $('#mw-content-text').append($(content));
+    });
+  });
+}
