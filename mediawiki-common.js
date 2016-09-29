39d56306a4d80c9eb732325619c4414d65ac7a4a
Manuel Huber <ManuelHu@users.noreply.github.com>
2016-09-29 19:09:18 +0200

    get image urls

--- mediawiki-common.js
+++ mediawiki-common.js
@@ -23,6 +23,12 @@ if ( mw.config.get( 'wgPageName' ) === 'Crew' ) {
         links.push('<a rel="nofollow" class="external text" href="'+url+'">'+title+'</a>');
       });
       
+      var image = $(this).find('PictureFilename').text();
+      if ( image.length ) {
+        var img_url = url_base + image + '.png';
+        console.log(img_url);
+      }
+      
       var content = '<h2><span class="mw-headline" id="'+username+'">'+username+'</span></h2>';
       content += '<p>'+description+'</p><p><b>Weiterführende Links:</b> '+links.join(' | ')+'<br />';
       content += '<b>Achievements:</b> '+achievements.join(' | ')+'</p>';
